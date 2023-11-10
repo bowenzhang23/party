0fe07259cb23e570aa5da918ab555923c0967a28
peppapiggyme <chambowen@126.com>
2023-11-10 16:20:31 +0800

    add base

--- src/party/vector.py
+++ src/party/vector.py
@@ -76,6 +76,23 @@ def theta2eta(theta: Any) -> np.float64:
     return -np.log(np.tan(theta * 0.5))
 
 
+def cos_theta(a: Any, b: Any) -> np.float64:
+    """Cosine angle between the two spatial vectors
+
+    Returns:
+        np.float64: cos_theta
+    """
+    assert a.shape == (3,) and b.shape == (3,)
+    norm_a = np.linalg.norm(a)
+    norm_b = np.linalg.norm(b)
+    if np.allclose(norm_a, 0, atol=1e-8) or np.allclose(norm_b, 0, atol=1e-8):
+        rnd = np.random.rand(1) * 2 - 1
+        return rnd.item()
+    u_a = a / norm_a
+    u_b = b / norm_b
+    return np.sum(u_a * u_b)
+
+
 # duality
 class Duality(object):
     """Vector dual space type
@@ -89,27 +106,22 @@ class Duality(object):
 
 
 # four vector class
-class FourVector(object):
+class FourVectorBase(object):
     def __init__(
         self,
-        t: Any,
-        x: Any,
-        y: Any,
-        z: Any,
+        vec: np.ndarray,
         label: str = "",
         duality: Duality = Duality.Contravariant,
     ) -> None:
         """Four Vector (Lorentz Vector)
 
         Args:
-            t (Any): time
-            x (Any): cartesian x
-            y (Any): cartesian y
-            z (Any): cartesian z
+            vec (np.ndarray): four vector
             label (str, optional): label of the variable for display. Defaults to "".
             duality (Duality, optional): contra- or covariant. Defaults to Duality.Contravariant.
         """
-        contra = np.array([t, x, y, z], dtype=np.float64)
+        contra = vec.copy().astype(np.float64).reshape(-1)
+        assert len(contra) == dim_spacetime, f"must satisfy len == {dim_spacetime}"
         if duality == Duality.Contravariant:
             self.txyz = contra
         elif duality == Duality.Covariant:
@@ -124,7 +136,7 @@ class FourVector(object):
         Returns:
             Any: zero vector
         """
-        return FourVector(*(0 for _ in range(dim_spacetime)))
+        return FourVectorBase(np.zeros((dim_spacetime,)))
 
     def __repr__(self) -> str:
         greek = r"u"
@@ -141,14 +153,14 @@ class FourVector(object):
         return self.txyz[i]
 
     def _copy(self) -> Any:
-        copy = FourVector.zero()
+        copy = FourVectorBase.zero()
         copy.txyz = self.txyz
         copy.label = self.label
         copy.duality = self.duality
         return copy
 
     def _binary_op(self, other: Any, op: str, fun_op: Callable) -> Any:
-        res: FourVector = FourVector.zero()
+        res: FourVectorBase = FourVectorBase.zero()
         res.label = (
             f"({self.label}"
             f"{op}"
@@ -176,7 +188,7 @@ class FourVector(object):
         Returns:
             Any: new four vector
         """
-        assert isinstance(other, FourVector), "must add with FourVector"
+        assert isinstance(other, FourVectorBase), "must add with FourVector"
         assert self.duality == other.duality, "must have same duality"
         return self._binary_op(other, "+", self.txyz.__add__)
 
@@ -189,7 +201,7 @@ class FourVector(object):
         Returns:
             Any: new four vector
         """
-        assert isinstance(other, FourVector), "must subtract with FourVector"
+        assert isinstance(other, FourVectorBase), "must subtract with FourVector"
         assert self.duality == other.duality, "must have same duality"
         return self._binary_op(other, "-", self.txyz.__sub__)
 
@@ -202,7 +214,7 @@ class FourVector(object):
         Returns:
             Any: scalar or four vector
         """
-        if isinstance(other, FourVector):
+        if isinstance(other, FourVectorBase):
             # dot product
             if self.duality == other.duality:
                 other = other.dual_transform()
@@ -230,13 +242,20 @@ class FourVector(object):
         ), "must be int or float"
         return self._binary_op(other, "x", self.txyz.__rmul__)
 
+    def copy(self) -> Any:
+        """
+        Returns:
+            Any: a copy of FourVector
+        """
+        return self._copy()
+
     def dual_transform(self) -> Any:
         """x_u = g_uv * x^v, x^u = g^uv * x_v
 
         Returns:
             Any: dual vector
         """
-        dual = FourVector.zero()
+        dual = FourVectorBase.zero()
         dual.label = self.label
         dual.duality = self.duality ^ 1
         dual.txyz = g_uv @ self.txyz
@@ -277,16 +296,24 @@ class FourVector(object):
         else:
             return self._boost_axis(velocity, param)
 
+    def boost_to_zmf_of(self, other: Any) -> Any:
+        """Boost to zero mass frame of a specific FourVector
+
+        Returns:
+            Any: transformed vector
+        """
+        E, p = other.txyz[0], other.txyz[1:]
+        b = p / E
+        v = np.linalg.norm(b)
+        return self.boost(v, tuple(b.tolist()))
+
     def boost_to_zmf(self) -> Any:
         """Boost to zero mass frame
 
         Returns:
             Any: transformed vector
         """
-        E, p = self.txyz[0], self.txyz[1:]
-        b = p / E
-        v = np.linalg.norm(b)
-        return self.boost(v, tuple(b.tolist()))
+        return self.boost_to_zmf_of(self)
 
     def _rotate_axis(self, angle: Any, axis: int) -> Any:
         """spatial rotation along a given axis
@@ -421,6 +448,40 @@ class FourVector(object):
         out.txyz = a_uv @ out.txyz
         out.label = f"boost({out.label}, axis={u}, velocity={velocity:.3f})"
         return out, a_uv
+
+    def cos_theta(self, other: Any) -> np.float64:
+        """Cosine angle between the spatial vector of two FourVectors
+
+        Args:
+            other (Any): the other four vector
+
+        Returns:
+            np.float64: cos_theta
+        """
+        return cos_theta(self.vector3(), other.vector3())
+
+
+class FourVector(FourVectorBase):
+    def __init__(
+        self,
+        t: Any,
+        x: Any,
+        y: Any,
+        z: Any,
+        label: str = "",
+        duality: Duality = Duality.Contravariant,
+    ) -> None:
+        """Four Vector (Lorentz Vector)
+
+        Args:
+            t (Any): time
+            x (Any): cartesian x
+            y (Any): cartesian y
+            z (Any): cartesian z
+            label (str, optional): label of the variable for display. Defaults to "".
+            duality (Duality, optional): contra- or covariant. Defaults to Duality.Contravariant.
+        """
+        super().__init__(np.array([t, x, y, z], dtype=np.float64), label, duality)
 
 
 class FourMomentum(FourVector):
